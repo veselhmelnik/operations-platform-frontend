3229085465d5ddcc2fe83c710f50649181caa26e
veselhmelnik <nikitadomitrak@gmail.com>
2026-09-10 13:46:35 +0300

    fix demo-only

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname
 
-  const demoOnly = process.env.DEMO_ONLY = 'true'
+  const demoOnly = process.env.DEMO_ONLY
 
   if (demoOnly) {
     const isDemoRoute = pathname === '/demo' || pathname.startsWith('/demo/')
